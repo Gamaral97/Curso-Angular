dae1073c88a1ddf0f107628642cf795afa0bc8f5
Gamaral97 <gabriel.amaral9761@gmail.com>
2025-09-24 19:38:04 -0300

    adicinado estilo padrão de layout

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayout } from '../../components/default-login-layout/default-login-layout';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   imports: [
-    DefaultLoginLayout
+    DefaultLoginLayout,
+    ReactiveFormsModule
   ],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
+  loginForm!: FormGroup;
+
+  constructor(){
+    this.loginForm = new FormGroup({
+      //email: new FormControl("",[Validators.required], Validators.email),
+      email: new FormControl<string>("", {nonNullable: true, validators: [Validators.required, Validators.email]}),
+      password: new FormControl<string>("", {nonNullable: true, validators: [Validators.required, Validators.minLength(6)]})
+    })
+  }
 
 }
